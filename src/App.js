00895fc9adfbdea39d0bb77ca16e74158ace2ac8
Lapenok <osharapova@yandex.com>
2022-04-03 23:11:44 +0300

    Square

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const GetUrlSquares = () => {
+    const [urls, setUrl] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/photos").then(result => result.json()).then((data) => {setUrl(data)});
+        
+    
+    },[]);
+
+    return (
+        <div>
+            {urls.map(el => (
+              <img key={el.id} src={el.url} alt ="" />
+            ))}
+        </div>
+
+    );
 }
-
-export default App;
